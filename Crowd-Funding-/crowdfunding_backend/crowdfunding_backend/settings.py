83d25fb4bcd4f56732d7861fcf58be89b7b18641
MaramEl-Sayed <maram20865@fci.bu.edu.eg>
2025-04-06 23:53:14 +0200

    edit settings.py

--- Crowd-Funding-/crowdfunding_backend/crowdfunding_backend/settings.py
+++ Crowd-Funding-/crowdfunding_backend/crowdfunding_backend/settings.py
@@ -95,13 +95,10 @@ TEMPLATES = [
         },
     },
 ]
-# AUTHENTICATION_BACKENDS = [
-#     "django.contrib.auth.backends.ModelBackend",
-#     "allauth.accounts.auth_backends.AuthenticationBackend",
-# ]
+
 AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
-    "allauth.account.auth_backends.AuthenticationBackend",  # Corrected path
+    "allauth.account.auth_backends.AuthenticationBackend",  
 ]
 WSGI_APPLICATION = "crowdfunding_backend.wsgi.application"
 
@@ -122,23 +119,6 @@ DATABASES = {
 }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
-
-# AUTH_PASSWORD_VALIDATORS = [
-#     {
-#         "NAME": "django.contrib.auth.password_validation.AccountsAttributeSimilarityValidator",
-#     },
-#     {
-#         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-#     },
-#     {
-#         "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-#     },
-#     {
-#         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-#     },
-# ]
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -186,7 +166,6 @@ REST_FRAMEWORK = {
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
